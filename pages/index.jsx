db5e2e8dbf770ffa84a98a7e3b38e7642f65e322
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-01-30 02:47:52 +0700

    dynamic data on index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,9 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
+import Property from "../components/Property";
+import { baseUrl, fetchApi } from "../utils/fetchApi";
+
 const Banner = ({
   purpose,
   title1,
@@ -35,7 +38,8 @@ const Banner = ({
   </Flex>
 );
 
-export default function Home() {
+export default function Home({ propertiesForSale, propertiesForRent }) {
+  console.log(propertiesForSale, propertiesForRent);
   return (
     <div>
       <Box>
@@ -50,7 +54,9 @@ export default function Home() {
           imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
         />
         <Flex flexWrap="wrap">
-          {/* Fetch the prperties and map over them ... */}
+          {propertiesForRent.map((property) => (
+            <Property property={property} key={property.id} />
+          ))}
         </Flex>
         <Banner
           purpose={"BUY A HOME"}
@@ -63,6 +69,27 @@ export default function Home() {
           imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
         />
       </Box>
+      <Flex flexWrap="wrap">
+        {propertiesForSale.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`
+  );
+  const propertyForRent = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`
+  );
+
+  return {
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+    },
+  };
 }
