5c82fe0b78f56e8c5b78abaddb1001c0d892cee0
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-01-30 17:04:26 +0700

    fix(utils): fixing rapid api key exposed to repository

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
       "x-rapidapi-host": "bayut.p.rapidapi.com",
-      "x-rapidapi-key": "4b4974357fmshf2e11eeea1c3738p1cd1aajsn7136a53af655",
+      "x-rapidapi-key": "{CHANGE TO YOUR KEY}",
     },
   });
 
